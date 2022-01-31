54703e0fd92c6dd335c2fd1ff12dcf2c919fd47f
henriqueIFPE <hlf@discente.ifpe.edu.br>
2022-01-31 18:27:44 -0300

    cadastro de doacao

--- src/main/java/br/ifpe/web3/controller/AdocaoController.java
+++ src/main/java/br/ifpe/web3/controller/AdocaoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.ifpe.web3.modelCadastro.Adocao;
 import br.ifpe.web3.service.AdocaoServices;
@@ -26,10 +27,18 @@ public class AdocaoController {
 	@GetMapping("/adocao")
 	public String adocao(Adocao adocao, ModelMap model ) {
 		
-		model.addAttribute("usuarios", this.usuarioService.findAll());
+		//model.addAttribute("usuarios", this.usuarioService.findAll());
 		model.addAttribute("pets", this.petServices.findAll());
 		
 		return "usuario/adocao";
+	}
+	
+	@PostMapping("/salvarAdocao")
+	public String salvarAdoacao(Adocao adocao) {
+		
+		this.adocaoServices.save(adocao);
+		
+		return "redirect:/adocao";
 	}
 	
 }
